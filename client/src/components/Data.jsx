b92636c3982a0eb321b3ba92541ca727b71e358f
patrick <patrick.weber@unifr.ch>
2024-01-18 19:35:41 +0100

    test

--- client/src/components/Data.jsx
+++ client/src/components/Data.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from "preact/hooks"
+import { useEffect, useState } from "preact/hooks";
 
-export default function Data({data}) {
-    const [msg,setMsg] = useState("Hello There")
- 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setMsg((prev) => prev + "Hello World!")
-    //     }, 1000)
-    // }, [])
+export default function Data({ data }) {
+  const [msg, setMsg] = useState("Hello There");
 
-    return (
+  // useEffect(() => {
+  //     setTimeout(() => {
+  //         setMsg((prev) => prev + "Hello World!")
+  //     }, 1000)
+  // }, [])
+
+  return (
     <>
-        <div className="output">{data.msg}</div>
-        <div className="output">Message: {msg}</div>
+      <div className="output">{data.msg}</div>
+      <div className="output">Message: {msg}</div>
     </>
-    )
+  );
 }
