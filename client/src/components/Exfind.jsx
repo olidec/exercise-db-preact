4eb8d30d59f12374cb193b50fe3dfd3cdd65dd17
olidec <69003898+olidec@users.noreply.github.com>
2024-01-22 15:50:15 +0100

    mapping all exercises test

--- client/src/components/Exfind.jsx
+++ client/src/components/Exfind.jsx
@@ -1,19 +1,28 @@
 import { useState, useEffect } from "preact/hooks";
 import { askServer } from "../utils/connector";
 
-
 export default function Exfind() {
-    const [ex, setEx] = useState([{id:0}]) 
+    const [ex, setEx] = useState([]) 
 
     const getEx = async () => {
         const res = await askServer("/api/ex","GET")
         setEx(res)
-        // console.log(res[1].content)
         const element = document.getElementById("exercise")
-        element.innerHTML = res[1].content
+
+        // for (let i = 0; i < res.length; i++) {
+        //     const element = document.createElement("li")
+        //     element.innerHTML = res[i].content
+        //     document.getElementById("exercise").appendChild(element)
+        // }
+
+        ex.map((ex) => {
+            const el = document.createElement("li")
+            el.innerHTML = ex.content
+            document.getElementById("exercise").appendChild(el)
+        })
+
         MathJax.typeset([element])
     }
-
 
     return (
         <>
