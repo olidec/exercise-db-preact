e963a9aeca23a208383470a04d0f414e3ef10dbf
olidec <69003898+olidec@users.noreply.github.com>
2024-01-27 20:41:22 +0100

    back to showing all exercises

--- client/src/components/Exfind.jsx
+++ client/src/components/Exfind.jsx
@@ -7,7 +7,7 @@ export default function Exfind() {
 
     const getEx = async () => {
         // const element = document.getElementById("exercise")
-        const res = await askServer("/api/ex/1","GET")
+        const res = await askServer("/api/ex/","GET")
         ex.value = res
         console.log(ex.value.content)
         // ex.value = res
@@ -18,16 +18,16 @@ export default function Exfind() {
         //     document.getElementById("exercise").appendChild(el)
         //     MathJax.typeset([el])
         // }
-        const el = document.createElement("li")
-        el.innerHTML = ex.value.content
-        document.getElementById("exercise").appendChild(el)
+        // const el = document.createElement("li")
+        // el.innerHTML = ex.value.content
+        // document.getElementById("exercise").appendChild(el)
 
-        // ex.map((ex) => {
-        //     const el = document.createElement("li")
-        //     el.innerHTML = ex.content
-        //     document.getElementById("exercise").appendChild(el)
-        //     MathJax.typeset([el])
-        // })
+        ex.value.map((ex) => {
+            const el = document.createElement("li")
+            el.innerHTML = ex.content
+            document.getElementById("exercise").appendChild(el)
+            MathJax.typeset([el])
+        })
 
         // MathJax.typeset([element])
     }
