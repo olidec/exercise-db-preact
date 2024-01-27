ffe8ec568b386a531f825550337569add3f3a70a
olidec <69003898+olidec@users.noreply.github.com>
2024-01-27 13:47:37 +0100

    trying signals

--- client/src/components/Exfind.jsx
+++ client/src/components/Exfind.jsx
@@ -1,25 +1,26 @@
 import { useState, useEffect } from "preact/hooks";
 import { askServer } from "../utils/connector";
+import { signal } from "@preact/signals";
 
 export default function Exfind() {
-    const [ex, setEx] = useState([]) 
+    const [ex, setEx] = signal([]) 
 
     const getEx = async () => {
-        const res = await askServer("/api/ex","GET")
-        setEx(res)
         const element = document.getElementById("exercise")
+        const res = await askServer("/api/ex","GET")
+        ex = res.value
 
-        // for (let i = 0; i < res.length; i++) {
-        //     const element = document.createElement("li")
-        //     element.innerHTML = res[i].content
-        //     document.getElementById("exercise").appendChild(element)
-        // }
+        for (let i = 0; i < ex.length; i++) {
+            const element = document.createElement("li")
+            element.innerHTML = ex[i].content
+            document.getElementById("exercise").appendChild(element)
+        }
 
-        ex.map((ex) => {
-            const el = document.createElement("li")
-            el.innerHTML = ex.content
-            document.getElementById("exercise").appendChild(el)
-        })
+        // res.map((ex) => {
+        //     const el = document.createElement("li")
+        //     el.innerHTML = ex.content
+        //     document.getElementById("exercise").appendChild(el)
+        // })
 
         MathJax.typeset([element])
     }
