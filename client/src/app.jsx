ecbc05d36f872cc169f35ff4aff51c2aa54090fc
patrick <patrick.weber@unifr.ch>
2024-02-11 15:40:23 +0100

    layout

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,17 +1,16 @@
-import { useEffect, useState } from "preact/hooks"
-import "./app.css"
-import { askServer } from "./utils/connector"
-import Data from "./components/Data"
-import Button from "./components/Button"
-import Form from "./components/Form"
-import Exform from "./components/Exform"
-import FindExAll from "./components/FindExAll"
-import FindExById from "./components/FindExById"
-import FindExByIdFromServer from "./components/FindExByIdFromServer"
-
+import { useEffect, useState } from "preact/hooks";
+import "./app.css";
+import { askServer } from "./utils/connector";
+import Data from "./components/Data";
+import Button from "./components/Button";
+import Form from "./components/Form";
+import Exform from "./components/Exform";
+import FindExAll from "./components/FindExAll";
+import FindExById from "./components/FindExById";
+import FindExByIdFromServer from "./components/FindExByIdFromServer";
+import Menu from "./components/Menu";
 export function App() {
-  const [data, setData] = useState({})
-  
+  const [data, setData] = useState({});
 
   // useEffect(async () => {
   //   const res = await askServer("/", "GET")
@@ -19,32 +18,35 @@ export function App() {
   // }, [])
 
   const getRoot = async () => {
-    const res = await askServer("/", "GET")
-    setData(res)
-  }
+    const res = await askServer("/", "GET");
+    setData(res);
+  };
   const getTest = async () => {
-    const res = await askServer("/api/test", "GET")
-    setData(res)
-  }
+    const res = await askServer("/api/test", "GET");
+    setData(res);
+  };
   const getSecret = async () => {
-    const res = await askServer("/api/secret", "POST", { pw: "password" })
-    setData(res)
-  }
+    const res = await askServer("/api/secret", "POST", { pw: "password" });
+    setData(res);
+  };
   const getWrongPassword = async () => {
-    const res = await askServer("/api/secret", "POST", { pw: "not this one" })
-    setData(res)
-  }
-
-  
-  
+    const res = await askServer("/api/secret", "POST", { pw: "not this one" });
+    setData(res);
+  };
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <Menu />
       <div>
-        <button className="pure-button" onClick={() => getTest()}>Get Test</button>
-        <button className="pure-button" onClick={() => getRoot()}>Get Root</button>
-        <button className="pure-button" onClick={() => getSecret()}>Get Secret</button>
+        <button className="pure-button" onClick={() => getTest()}>
+          Get Test
+        </button>
+        <button className="pure-button" onClick={() => getRoot()}>
+          Get Root
+        </button>
+        <button className="pure-button" onClick={() => getSecret()}>
+          Get Secret
+        </button>
         <button className="pure-button" onClick={() => getWrongPassword()}>
           Get Secret (wrong password)
         </button>
@@ -53,18 +55,18 @@ export function App() {
       <Data data={data} />
       <Button />
       <div id="user-form">
-      <Form />
+        <Form />
       </div>
       <hr />
+      <hr />
       <div id="ex-form">
-      <Exform />
+        <Exform />
       </div>
       <FindExById />
 
       <FindExAll />
 
       <FindExByIdFromServer />
-
     </>
-  )
+  );
 }
--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -2,53 +2,94 @@ import { useState } from "preact/hooks";
 import { askServer } from "../utils/connector";
 
 export default function Form() {
-    const [user,setUser] = useState({
-        email: '',
-        name:'',
-        password:''
-    })
-    
-    const addNewUser = async (e) => {
-        e.preventDefault()
-        console.log("hallo")
-        const res = await askServer("/api/user","POST",user)
-        console.log(res)
-        if (res.err) {
-            console.log(res.err)
-        }
-        else {
-            setUser({
-                email: '',
-                name:'',
-                password:''
-            });
-            console.log("user added")
-        }
+  const [user, setUser] = useState({
+    email: "",
+    name: "",
+    password: "",
+  });
+
+  const addNewUser = async (e) => {
+    e.preventDefault();
+    console.log("hallo");
+    const res = await askServer("/api/user", "POST", user);
+    console.log(res);
+    if (res.err) {
+      console.log(res.err);
+    } else {
+      setUser({
+        email: "",
+        name: "",
+        password: "",
+      });
+      console.log("user added");
     }
+  };
 
-    const updateUserHandler = (e) => {
-        e.preventDefault()
-        const { name, value } = e.target;
-        setUser((prevUser) => ({
-            ...prevUser,
-            [name]: value
-        }));
-    }
+  const updateUserHandler = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
 
-    return (
-        <>
-        <form className="pure-form" onSubmit={(e) => addNewUser(e)}>
-            <legend>Add a new User</legend>
-            {/* <label htmlFor="email">E-Mail</label> */}
-            <input type="email" name="email" id="email" value={user.email} onChange={updateUserHandler} placeholder="E-Mail"/>
-            {/* <label htmlFor="name">Name:</label> */}
-            <input type="text" name="name" id="name" value={user.name} onChange={updateUserHandler} placeholder="Name"/>
-            {/* <label htmlFor="password">Password:</label> */}
-            <input type="password" name="password" id="password" value={user.password} onChange={updateUserHandler} placeholder="Password"/>
-            <button className="pure-button pure-button-primary" type="submit">Add new user</button>
-        </form>
-        </>
-    )
+  return (
+    <>
+      <form
+        className="pure-form pure-form-aligned"
+        onSubmit={(e) => addNewUser(e)}
+      >
+        <legend>Add a new User</legend>
+        <fieldset>
+          <div class="pure-control-group">
+            <label for="aligned-email">Email </label>
 
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={user.email}
+              onChange={updateUserHandler}
+              placeholder="E-Mail"
+            />
+            <span class="pure-form-message-inline">
+              This is a required field.
+            </span>
+          </div>
 
+          <div class="pure-control-group">
+            <label for="aligned-name">Username </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={user.name}
+              onChange={updateUserHandler}
+              placeholder="Name"
+            />
+          </div>
+
+          <div class="pure-control-group">
+            <label for="aligned-password">Password </label>
+
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={user.password}
+              onChange={updateUserHandler}
+              placeholder="Password"
+            />
+          </div>
+
+          <div class="pure-controls">
+            <button className="pure-button pure-button-primary" type="submit">
+              Add new user
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    </>
+  );
 }
