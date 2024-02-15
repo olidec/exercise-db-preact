c2dbbabc1b622811f9c8450cf155556292ada9de
olidec <69003898+olidec@users.noreply.github.com>
2024-02-15 16:16:50 +0100

    switched to signals

--- client/src/components/Exform.jsx
+++ client/src/components/Exform.jsx
@@ -1,26 +1,45 @@
-import { useState } from "preact/hooks";
+// import { useState } from "preact/hooks";
+import { signal } from "@preact/signals";
 import { askServer } from "../utils/connector";
+import { cat, loadCat } from "../signals/categories.js";
+import { useEffect } from "react";
 
 export default function Form() {
-    const [ex, setEx] = useState({
-        summary: '',
+    useEffect(() => {
+        loadCat()
+    },[])
+    
+    console.log(cat.value)
+
+    const ex = signal({
         content:'',
-        solution:''
+        solution:'',
+        categories:[],
     })
+
+
+    // cat.value.map((c, key) => {
+    //     const el = document.createElement("input")
+    //     el.setAttribute("type","checkbox")
+    //     el.setAttribute("key", key)
+    //     el.innerHTML = c.name
+    //     el.value = c.name
+    //     document.getElementById("catgroupexform").appendChild(el)
+    // })
     
     const addNewEx = async (e) => {
         e.preventDefault()
-        const res = await askServer("/api/ex","POST",ex)
-        console.log(res)
+        const res = await askServer("/api/ex","POST",ex.value)
+        // console.log(res)
         if (res.err) {
             console.log(res.err)
         }
         else {
-            setEx({
-                summary: '',
+            ex.value = {
                 content:'',
-                solution:''
-            });
+                solution:'',
+                categories:[],
+            };
             console.log("exercise added")
         }
     }
@@ -28,31 +47,52 @@ export default function Form() {
     const updateExHandler = (e) => {
         e.preventDefault()
         const { name, value } = e.target;
-        setEx((prevEx) => ({
-            ...prevEx,
-            [name]: value
-        }));
+        ex.value[name] = value
+        // console.log(ex.value)
+    }
+
+    const toggle = (e) => {
+        e.preventDefault()
+        const { value } = e.target
+        if (ex.value.categories.includes(value)) {
+            ex.value.categories = ex.value.categories.filter((c) => c !== value)
+        }
+        else {
+            ex.value.categories.push(value)
+        }
+        console.log(ex.value)
     }
 
     return (
         <>
         <form className="pure-form pure-form-aligned" onSubmit={(e) => addNewEx(e)}>
-            <legend>Add your own Exercise</legend>
+            <legend>Füge deine eigene Aufgabe hinzu.</legend>
             <fieldset>
-                <div className="pure-control-group">
-                    <label htmlFor="summary">Summary: </label>
-                        <textarea rows={2} cols={100} type="text" name="summary" id="summary" value={ex.summary} onChange={updateExHandler} placeholder="Write a short summary of you exercise"/>
+            <div className="pure-control-group">
+                <label htmlFor="content">Aufgabentext: </label>
+                <textarea rows="5" cols="100" name="content" id="content" value={ex.value.content} onChange={updateExHandler} placeholder="Gib den Aufgabentext im LaTeX Format ein"/>
+            </div>
+            <div className="pure-control-group">
+            <label htmlFor="solution">Lösung:</label>
+                <textarea rows="5" cols="100" name="solution" id="solution" value={ex.value.solution} onChange={updateExHandler} placeholder="Gib die Lösung im LaTeX Format ein"/>
+            </div>
+            <div className="pure-control-group">
+                <label htmlFor="categories">Kategorien:</label>
+                <div id="catgroupexform">
+                {cat.value.map((c, key) => {
+                    return (
+                        <div key={key}>
+                            <label htmlFor={c.name}>{c.name}</label>
+                            <input type="checkbox" id={c.name} name="categories" value={c.id} onChange={toggle}/>
+                        </div>
+                    )
+                })}
                 </div>
-            <div className="pure-control-group">
-                <label htmlFor="content">Exercise Text: </label>
-                    <textarea rows="5" cols="100" name="content" id="content" value={ex.content} onChange={updateExHandler} placeholder="Enter the exercise text in the LaTeX format"/>
+
             </div>
-            <div className="pure-control-group">
-            <label htmlFor="solution">Solution: </label>
-                <textarea rows="5" cols="100" name="solution" id="solution" value={ex.solution} onChange={updateExHandler} placeholder="Enter the exercise solution in the LaTeX format"/>
-            </div>
+        
             <div className="pure-controls">
-                <button type="submit" className="pure-button pure-button-primary">Add new exercise</button>
+                <button type="submit" className="pure-button pure-button-primary">Aufgabe hinzufügen</button>
             </div>
             </fieldset>
         </form>
