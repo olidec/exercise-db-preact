fba7978617d712303a33fb6b40ab7525ab44f0bb
patrick <patrick.weber@unifr.ch>
2024-02-15 16:16:16 +0100

    Karten erstellt

--- client/src/components/FindExBySearchText.jsx
+++ client/src/components/FindExBySearchText.jsx
@@ -1,76 +1,58 @@
 import { askServer } from "../utils/connector";
+import { useState, useEffect } from "preact/hooks";
 import { signal } from "@preact/signals";
-
+import Card from "./Card";
+import CardList from "./CardList";
 export default function FindExBySearchText() {
-    const searchText = signal("")
-    const exerciseList = signal([])
+  const searchText = signal("");
+  const [exerciseList, setExerciseList] = useState([]);
 
-    const onChange = (e) => {
-        e.preventDefault()
-        const { value } = e.target
-        searchText.value = value
-        // unccoment line below to update view immediately on change
-        // getEx(e)
+  const onChange = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    searchText.value = value;
+    // unccoment line below to update view immediately on change
+    // getEx(e)
+  };
+
+  const getEx = async (e) => {
+    e.preventDefault();
+    const route = `/api/ex?search=${searchText.value}`;
+    const res = await askServer(route, "GET");
+    if (res.errors || res.length === 0) {
+      alert("No exercises match the search term.");
+      return;
+    } else {
+      setExerciseList(res);
     }
+  };
 
-    const getEx = async (e) => {
-        e.preventDefault()
-        const route = `/api/ex?search=${searchText.value}`
-        // console.log(route)
-        const res = await askServer(route,"GET")
-        // console.log(res)
-        if (res.errors || res.length === 0) {
-            alert("No exercises match the search term.")
-            return
-        }
-        else {
-            exerciseList.value = res
-        }
-        // console.log(exerciseList.value)
-        exsearchlist.innerHTML = ""
-        exerciseList.value.map((ex,key) => {
-            const el = document.createElement("tr")
-            el.setAttribute("key",key)
-            el.innerHTML = `<td>${ex.id}</td><td>${ex.content}</td><td>${ex.solution}</td>`
-            exsearchlist.appendChild(el)
-            MathJax.typeset([el])
-        })
-        
-        // console.log(res.content)
-        // res.map((ex) => {
-        //     const el = document.createElement("tr")
-        //     el.innerHTML = `<td>${ex.id}</td><td>${ex.content}</td><td>${ex.solution}</td>`
-        //     textsearchtable.appendChild(el)
-        //     MathJax.typeset([el])
-        // })
-        
+  const handleDelete = (index) => {
+    const updatedList = [...exerciseList];
+    updatedList.splice(index, 1);
+    setExerciseList(updatedList);
+    MathJax.typeset();
+  };
 
-        // myexidnew.innerHTML = res.id
-        // myexcontnew.innerHTML = res.content
-        // myexsolnew.innerHTML = res.solution
-        
-    }
+  useEffect(() => {
+    MathJax.typeset();
+  }, [exerciseList]);
 
-    return (
-        <>
-            <form onSubmit={(e) => getEx(e)}>
-                <label htmlFor="exid-3">Search Exercises for contents</label>
-            <input id="exid-3" value={searchText} onChange={onChange}/>
-            <button className="pure-button">Find Exercises containing</button>
-            </form>
-            <div>
-                <table id="exfromtext" className="pure-table pure-table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Content</th>
-                            <th>Solution</th>
-                        </tr>
-                    </thead>
-                    <tbody id="exsearchlist">                        
-                    </tbody>
-                </table>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <form onSubmit={(e) => getEx(e)}>
+        <label htmlFor="exid-3">Search Exercises for contents</label>
+        <input id="exid-3" value={searchText} onChange={onChange} />
+        <button className="pure-button">Find Exercises containing</button>
+      </form>
+
+      <div id="exsearchlist">
+        <CardList
+          cards={exerciseList.map((ex, index) => (
+            <Card key={index} content={ex.content} summary={ex.summary} />
+          ))}
+        />
+      </div>
+    </>
+  );
 }
