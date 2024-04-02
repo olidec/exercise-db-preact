883211555a385af5ba02b09ef1da99d48acc1450
olidec <69003898+olidec@users.noreply.github.com>
2024-04-02 15:41:44 +0200

    manuelle Lösung - nicht optimal, aber es funktioniert...

--- client/src/components/Exform.jsx
+++ client/src/components/Exform.jsx
@@ -1,7 +1,6 @@
 import { h } from "preact";
 import { useState, useEffect } from "preact/hooks";
 import { askServer } from "../utils/connector";
-import { cat, loadCat } from "../signals/categories.js";
 
 export default function ExForm() {
     const [ex, setEx] = useState({
@@ -9,43 +8,48 @@ export default function ExForm() {
         content:'',
         solution:''
     })
-    const categories = []
-    const subcategories = [] 
 
-    const [selectedCategory, setSelectedCategory] = useState(null);
-    const [selectedSubcategory, setSelectedSubcategory] = useState(null);
     
+    let categories = ['-- Wähle bitte eine Kategorie --','Zahlen', 'Arithmetik und Algebra', 'Geometrie', 'Analysis', 'Stochastik', 'Vertiefende Themen']
+    const subcategories = [['-- Wähle bitte zuerst eine Kategorie --'],
+    ['-- Wähle bitte eine Unterkategorie --','Zahlensysteme', 'Spezielle Zahlen', 'Zahlenmengen', 'Sonstiges'], 
+    ['-- Wähle bitte eine Unterkategorie --','Rechenarten', 'Gleichungen und Ungleichungen', 'Funktionen', 'Sonstiges'],
+    ['-- Wähle bitte eine Unterkategorie --','Allgemeine Geometrie der Ebene', 'Trigonometrie', 'Allgemeine Geometrie des Raums', 'Vektorgeometrie', 'Sonstiges'],
+    ['-- Wähle bitte eine Unterkategorie --','Grundlagen', 'Differentialrechnung', 'Integralrechnung', 'Sonstiges'],
+    ['-- Wähle bitte eine Unterkategorie --','Wahrscheinlichkeitstheorie', 'Kombinatorik', 'Statistik', 'Sonstiges'],
+    ['-- Wähle bitte eine Unterkategorie --','Komplexe Zahlen', 'Kegelschnitte', 'Differentialgleichungen', 'Lineare Abbildungen und Matrizen', 'Graphentheorie', 'Sonstiges']] 
+
+    const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+    const [selectedSubcategory, setSelectedSubcategory] = useState(subcategories[0][0]);
+
+    
+
+
+
+    
+
+
+
+    // selectedCategory.value = categories[0];
+    // selectedSubcategory.value =  subcategories[0][0];
+
+    // console.log(selectedCategory)
+    // console.log(selectedSubcategory.value)
+
+
     useEffect(() => {
-        const fetchCat = async () => {
-            await loadCat();
-            cat.value.map((c) => {
-                categories.push(c.name)
-            })
-            cat.value.map((c) => {
-            const temp = []
-            c.subcategory.map((s) => {
-                temp.push(s.name)
-            })
-            subcategories.push(temp)
-            })
-            setSelectedCategory(categories[0]);
-            setSelectedSubcategory(subcategories[0][0]);
-        
-            // console.log(cat.value)
-            // console.log(categories)
-            // console.log(subcategories)
-
-        }
-        fetchCat();
-    }, []);
-
-            
-
+        setSelectedSubcategory(subcategories[categories.indexOf(selectedCategory)][0]);
+    }, [selectedCategory]);
 
 
     
     const addNewEx = async (e) => {
         e.preventDefault()
+        const exWithCategory = {
+            ...ex,
+            category: selectedCategory,
+            subcategory: selectedSubcategory
+        };
         const res = await askServer("/api/ex","POST",ex)
         console.log(res)
         if (res.err) {
@@ -75,11 +79,23 @@ export default function ExForm() {
             <legend>Füge deine eigene Aufgabe hinzu</legend>
             <fieldset>
             <div className="pure-control-group">
-                <label htmlFor="content">Exercise Text: </label>
+                <label htmlFor="category">Kategorie: </label>
+            <select name="category" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+            {categories.map((category, index) => <option key={index} value={category}>{category}</option>)}
+            </select>
+            </div>
+            <div className="pure-control-group">
+            <label htmlFor="subcategory">Unterkategorie: </label>
+            <select name="subcategory" value={selectedSubcategory} onChange={(e) => setSelectedSubcategory(e.target.value)}>
+                {subcategories[categories.indexOf(selectedCategory)].map((subcategory, index) => <option key={index} value={subcategory}>{subcategory}</option>)}
+            </select>
+            </div>
+            <div className="pure-control-group">
+                <label htmlFor="content">Aufgabentext: </label>
                     <textarea rows="5" cols="100" name="content" id="content" value={ex.content} onChange={updateExHandler} placeholder="Schreibe deine Aufgabe im LaTeX Format"/>
             </div>
             <div className="pure-control-group">
-            <label htmlFor="solution">Solution: </label>
+            <label htmlFor="solution">Lösung: </label>
                 <textarea rows="5" cols="100" name="solution" id="solution" value={ex.solution} onChange={updateExHandler} placeholder="Schreibe deine Lösung im LaTeX Format"/>
             </div>
             <div className="pure-controls">
