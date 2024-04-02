62848dc440b83658fc9c80499bfa89a5bf9572e1
olidec <69003898+olidec@users.noreply.github.com>
2024-04-02 12:36:40 +0200

    neu auf Deutsch und "summary" entfernt

--- client/src/components/Exform.jsx
+++ client/src/components/Exform.jsx
@@ -37,19 +37,19 @@ export default function Form() {
     return (
         <>
         <form className="pure-form pure-form-aligned" onSubmit={(e) => addNewEx(e)}>
-            <legend>Add your own Exercise</legend>
+            <legend>Füge deine eigene Aufgabe hinzu</legend>
             <fieldset>
-                <div className="pure-control-group">
+                {/* <div className="pure-control-group">
                     <label htmlFor="summary">Summary: </label>
                         <textarea rows={2} cols={100} type="text" name="summary" id="summary" value={ex.summary} onChange={updateExHandler} placeholder="Write a short summary of you exercise"/>
-                </div>
+                </div> */}
             <div className="pure-control-group">
                 <label htmlFor="content">Exercise Text: </label>
-                    <textarea rows="5" cols="100" name="content" id="content" value={ex.content} onChange={updateExHandler} placeholder="Enter the exercise text in the LaTeX format"/>
+                    <textarea rows="5" cols="100" name="content" id="content" value={ex.content} onChange={updateExHandler} placeholder="Schreibe deine Aufgabe im LaTeX Format"/>
             </div>
             <div className="pure-control-group">
             <label htmlFor="solution">Solution: </label>
-                <textarea rows="5" cols="100" name="solution" id="solution" value={ex.solution} onChange={updateExHandler} placeholder="Enter the exercise solution in the LaTeX format"/>
+                <textarea rows="5" cols="100" name="solution" id="solution" value={ex.solution} onChange={updateExHandler} placeholder="Schreibe deine Lösung im LaTeX Format"/>
             </div>
             <div className="pure-controls">
                 <button type="submit" className="pure-button pure-button-primary">Add new exercise</button>
