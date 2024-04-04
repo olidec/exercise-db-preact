1d36208a420022c7ac917c1914f23bfd4152a438
olidec <69003898+olidec@users.noreply.github.com>
2024-04-04 17:50:46 +0200

    added download button

--- client/src/components/Warenkorb.jsx
+++ client/src/components/Warenkorb.jsx
@@ -5,18 +5,34 @@ import Menu from "./Menu.jsx";
 import { WarenkorbContext } from "../signals/warenkorb.jsx";
 import { useContext } from "preact/hooks";
 import CardListWaren from "./CardListWaren.jsx";
+import { askServer } from "../utils/connector";
 
-const Warenkorb = ({}) => {
-  const { cartItems, getCart, getCartCount } = useContext(WarenkorbContext);
+const Warenkorb = () => {
+  const { cartItems, getCartCount, arrayIDs } = useContext(WarenkorbContext);
+
+  const getDownload = async () => {
+    try {
+      const route = "/api/download";
+      const method = "POST";
+      const body = { exerciseIds: arrayIDs }; // Daten, die gesendet werden sollen
+
+      const res = await askServer(route, method, body);
+
+      console.log(res);
+    } catch (error) {
+      console.error("Fehler beim Herunterladen der Übungen:", error);
+    }
+  };
 
   return (
     <>
-      <div className="inhalt">
       <h1>Warenkorb ({getCartCount()})</h1>
+      <button className="pure-button" onClick={getDownload}>
+        Warenkorb downloaden
+      </button>
       <hr />
       <div>
         <CardListWaren list={cartItems.value} />
-      </div>
       </div>
     </>
   );
