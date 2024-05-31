9cb2630b3347e4ec2090909fec70038222f84349
patrick <patrick.weber@unifr.ch>
2024-05-31 11:20:50 +0200

    style Menue

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,8 +1,15 @@
 import { Link } from "preact-router/match";
 import { useContext, useState } from "preact/hooks";
 import { SearchContext } from "../signals/exercise.jsx";
+import { WarenkorbContext } from "../signals/warenkorb.jsx";
 export default function Menu() {
-  const { cartSearch, getCartSearch } = useContext(SearchContext);
+  const { cartSearch } = useContext(SearchContext);
+  const [selectedMenu, setSelectedMenu] = useState("");
+  const { cartItems, getCart, getCartCount } = useContext(WarenkorbContext);
+  const handleMenuClick = (menu) => {
+    setSelectedMenu(menu);
+  };
+
   return (
     <div
       className="pure-menu pure-menu-horizontal"
@@ -22,27 +29,56 @@ export default function Menu() {
           width: "100%",
         }}
       >
-        <li className="pure-menu-item pure-menu-selected">
-          <Link href="/exercise-db-preact/user" className="pure-menu-link">
+        <li
+          className={`pure-menu-item ${
+            selectedMenu === "user" ? "pure-menu-selected" : ""
+          }`}
+        >
+          <Link
+            href="/exercise-db-preact/user"
+            className="pure-menu-link"
+            onClick={() => handleMenuClick("user")}
+          >
             User
           </Link>
         </li>
-        <li className="pure-menu-item pure-menu-selected">
-          <Link href="/exercise-db-preact/add" className="pure-menu-link">
+        <li
+          className={`pure-menu-item ${
+            selectedMenu === "add" ? "pure-menu-selected" : ""
+          }`}
+        >
+          <Link
+            href="/exercise-db-preact/add"
+            className="pure-menu-link"
+            onClick={() => handleMenuClick("add")}
+          >
             Aufgaben hinzufügen
           </Link>
         </li>
-        <li className="pure-menu-item pure-menu-selected">
-          <Link href="/exercise-db-preact/find" className="pure-menu-link">
+        <li
+          className={`pure-menu-item ${
+            selectedMenu === "find" ? "pure-menu-selected" : ""
+          }`}
+        >
+          <Link
+            href="/exercise-db-preact/find"
+            className="pure-menu-link"
+            onClick={() => handleMenuClick("find")}
+          >
             Aufgaben finden
           </Link>
         </li>
         {cartSearch.value.length !== 0 ? (
-          <li className="pure-menu-item pure-menu-selected">
+          <li
+            className={`pure-menu-item ${
+              selectedMenu === "search" ? "pure-menu-selected" : ""
+            }`}
+          >
             <Link
               href="/exercise-db-preact/search"
               className="pure-menu-link"
-              style={{ color: "purple" }}
+              style={{ color: "black" }}
+              onClick={() => handleMenuClick("search")}
             >
               Letzte Suchresultate
             </Link>
@@ -50,24 +86,33 @@ export default function Menu() {
         ) : null}
 
         <li
-          className="pure-menu-item pure-menu-selected"
+          className={`pure-menu-item ${
+            selectedMenu === "warenkorb" ? "pure-menu-selected" : ""
+          }`}
           style={{ marginLeft: "auto" }}
         >
           <div id="notification-container"></div>
           <div id="checkmark">✔</div>
 
-          <Link href="/exercise-db-preact/warenkorb" className="pure-menu-link">
-            Warenkorb
+          <Link
+            href="/exercise-db-preact/warenkorb"
+            className="pure-menu-link"
+            onClick={() => handleMenuClick("warenkorb")}
+          >
+            Warenkorb ({getCartCount()})
           </Link>
         </li>
         <li
-          className="pure-menu-item pure-menu-has-children pure-menu-allow-hover"
+          className={`pure-menu-item pure-menu-has-children pure-menu-allow-hover ${
+            selectedMenu === "contact" ? "pure-menu-selected" : ""
+          }`}
           style={{ marginLeft: "auto", display: "none" }}
         >
           <Link
             href="/exercise-db-preact/contact"
             id="menuLink1"
             className="pure-menu-link"
+            onClick={() => handleMenuClick("contact")}
           >
             Contact
           </Link>
@@ -76,6 +121,7 @@ export default function Menu() {
               <Link
                 href="/exercise-db-preact/contact"
                 className="pure-menu-link"
+                onClick={() => handleMenuClick("contact")}
               >
                 Email
               </Link>
@@ -84,6 +130,7 @@ export default function Menu() {
               <Link
                 href="/exercise-db-preact/contact"
                 className="pure-menu-link"
+                onClick={() => handleMenuClick("contact")}
               >
                 Twitter
               </Link>
@@ -92,6 +139,7 @@ export default function Menu() {
               <Link
                 href="/exercise-db-preact/contact"
                 className="pure-menu-link"
+                onClick={() => handleMenuClick("contact")}
               >
                 Tumblr Blog
               </Link>
