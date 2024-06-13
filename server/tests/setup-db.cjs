16fac189b5a35debd95dc41f527eda61d1b78a0f
olidec <69003898+olidec@users.noreply.github.com>
2024-06-13 12:54:27 +0200

    db backup

--- server/tests/setup-db.cjs
+++ server/tests/setup-db.cjs
@@ -1,21 +1,32 @@
-const PrismaClient = require("@prisma/client")
-const prisma = new PrismaClient.PrismaClient()
+const PrismaClient = require("@prisma/client");
+const prisma = new PrismaClient.PrismaClient();
+
+// npx prisma generate
+// npx prisma migrate dev --name init
+// node server/tests/setup-db.cjs
+
+// resp. durch
+// npm run db-setup
+// ersetzen
+
+// backup db:
+// sqlite ./dev.db .output ./backup.sql .dump .exit
 
 async function setup() {
-    await prisma.subcategory.update({
-        where: { id: 20 },
-        data: { 
-            subsubcategory: {
-                create: [
-                        { name: "Bedingte Wahrscheinlichkeit" },
-                        { name: "Verteilungen" },
-                        { name: "Erwartungswert" },
-                    ]
-            }
-        },
-        include: { subsubcategory: true }
-    })
-    console.log("done")
+  await prisma.subcategory.update({
+    where: { id: 20 },
+    data: {
+      subsubcategory: {
+        create: [
+          { name: "Bedingte Wahrscheinlichkeit" },
+          { name: "Verteilungen" },
+          { name: "Erwartungswert" },
+        ],
+      },
+    },
+    include: { subsubcategory: true },
+  });
+  console.log("done");
 }
 
-setup()
+setup();
