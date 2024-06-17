f1b6d8ca3664cfe2c8065137501d42a8a6a31eb3
olidec <69003898+olidec@users.noreply.github.com>
2024-06-17 12:16:45 +0200

    refactor route:register

--- server/controllers/users.cjs
+++ server/controllers/users.cjs
@@ -1,24 +1,52 @@
 const PrismaClient = require("@prisma/client");
 const prisma = new PrismaClient.PrismaClient();
+const argon2 = require("argon2");
+
+/**
+ * Erstelle einen neuen User in der Datenbank
+ * @param {*} username
+ * @param {*} email
+ * @param {*} password
+ * @returns
+ */
+async function createUser(username, email, password) {
+  const hashedPassword = await argon2.hash(password);
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      hashedPassword,
+    },
+  });
+  return user;
+}
+
+/**
+ * hole User aus der Datenbank
+ * @param {*} options
+ * @returns
+ */
+async function getUser(options = { id, username, email }) {
+  let user = null;
+  if (options.id) {
+    user = await prisma.user.findUnique({
+      where: { id: options.id },
+    });
+  } else if (options.username) {
+    user = await prisma.user.findUnique({
+      where: { username: options.username },
+    });
+  } else if (options.email) {
+    user = await prisma.user.findUnique({
+      where: { email: options.email },
+    });
+  } else throw new Error("No user identifier provided", options);
+
+  if (user) return { mgs: "User found", user, success: true };
+  else return { msg: "User not found", success: false };
+}
 
 module.exports = {
-  getUser: async (options = { id, username, email }) => {
-    let user = null;
-    if (options.id) {
-      user = await prisma.user.findUnique({
-        where: { id: options.id },
-      });
-    } else if (options.username) {
-      user = await prisma.user.findUnique({
-        where: { username: options.username },
-      });
-    } else if (options.email) {
-      user = await prisma.user.findUnique({
-        where: { email: options.email },
-      });
-    } else throw new Error("No user identifier provided", options);
-
-    if (user) return { mgs: "User found", user, success: true };
-    else return { msg: "User not found", success: false };
-  },
+  getUser: getUser,
+  createUser: createUser,
 };
--- server/server.cjs
+++ server/server.cjs
@@ -17,7 +17,8 @@ const router = express.Router();
 const argon2 = require("argon2");
 
 const { initializePassport } = require("./passport-config.cjs");
-const { getUser } = require("./controllers/users.cjs");
+const { getUser, createUser } = require("./controllers/users.cjs");
+const { create } = require("domain");
 initializePassport(passport);
 
 app.use(express.urlencoded({ extended: false }));
@@ -81,31 +82,22 @@ router.delete("/logout", (req, res) => {
 });
 
 router.post("/register", async (req, res) => {
-  console.log(req.body);
   // TODO input validation
   const { email, username, password } = req.body;
-  const hashedPassword = await argon2.hash(password);
+
   try {
     // ACHTUNG nur ein Feld wird überprüft
     // schaue getUser an für Reihenfolge
     // TODO write checkUser function
-    const { msg, user, success } = await getUser({
+    const { success } = await getUser({
       email: email,
       username: username,
     });
-    console.log("ROUTE [REGISTER]:", msg, user, success);
+
     if (success) {
       res.json({ msg: "User already exists", err: "User already exists" });
-      return;
     } else {
-      const newUser = await prisma.user.create({
-        data: {
-          email: email,
-          username: username,
-          password: hashedPassword,
-        },
-      });
-      console.log("ROUTE [REGISTER]:", newUser);
+      const newUser = await createUser(username, email, password);
       res.json({
         msg: "User created successfully",
         data: {
