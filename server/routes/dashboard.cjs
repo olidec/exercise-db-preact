e6524f7967d254b213822ead52a93bb67a947100
olidec <69003898+olidec@users.noreply.github.com>
2024-06-17 14:16:32 +0200

    edit

--- server/routes/dashboard.cjs
+++ server/routes/dashboard.cjs
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-// does not "dashboard" in route url already comes from routes/index.cjs
+// does not need "dashboard" in route url already comes from routes/index.cjs
 router.get("/", (req, res) => {
   console.log(req.isAuthenticated());
   return res.json({
