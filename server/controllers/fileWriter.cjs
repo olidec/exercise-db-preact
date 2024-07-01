b7f288c20e223f900f95eef61f99e0cb7a4677f9
olidec <69003898+olidec@users.noreply.github.com>
2024-07-01 15:41:55 +0200

    added documentation to fileWriter

--- server/controllers/fileWriter.cjs
+++ server/controllers/fileWriter.cjs
@@ -1,4 +1,10 @@
+// needed to use fs/promises to write to file otherwise downloaded file was empty
 const fs = require("node:fs/promises");
+
+/**
+ * Schreibe JSON in eine Datei
+ * @param {Array} json - JSON-Array
+ */
 
 async function fileWriter(json) {
     console.log("Writing to file");
@@ -13,7 +19,7 @@ async function fileWriter(json) {
             } else {
             console.log("Data written to file successfully");
             }
-        });
+    });
 }
 
 module.exports = fileWriter;
