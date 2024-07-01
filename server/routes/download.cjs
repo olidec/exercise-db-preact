f6438cf504604b8a207ec1ed2203a676ac5c7af1
olidec <69003898+olidec@users.noreply.github.com>
2024-07-01 14:00:53 +0200

    added exercises controller to download route

--- server/routes/download.cjs
+++ server/routes/download.cjs
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 const fs = require("fs");
-const PrismaClient = require("@prisma/client");
-const prisma = new PrismaClient.PrismaClient();
 
 const {getRecentExercises, getExercisesByIds }  = require("../controllers/exercises.cjs");
 
@@ -37,42 +35,11 @@ try {
     const { exerciseIds } = req.body;
     console.log(exerciseIds);
     try {
-      const exercises = await prisma.exercise.findMany({
-        where: {
-          id: {
-            in: exerciseIds,
-          },
-        },
-      });
-      const sortedExercises = exerciseIds.map((id) => {
-        const exercise = exercises.find((ex) => ex.id === id);
-        return exercise;
-      });
+      const sortedExercises = await getExercisesByIds(exerciseIds);
   
       console.log(sortedExercises);
       return res.json(sortedExercises);
-      // const writeToFile = (exercises) => {
-      //   const contentAndSolution = exercises.map((exercise) => ({
-      //     content: exercise.content,
-      //     solution: exercise.solution,
-      //   }));
-  
-      //   fs.writeFile(
-      //     "/path/to/output.txt",
-      //     JSON.stringify(contentAndSolution),
-      //     (err) => {
-      //       if (err) {
-      //         console.error("Error writing to file:", err);
-      //       } else {
-      //         console.log("Data written to file successfully");
-      //         res.download("/path/to/output.txt", "output.txt");
-      //       }
-      //     }
-      //   );
-      // };
-  
-      // writeToFile(exercises);
-      // res.json(exercises);
+
     } catch (error) {
       console.error("Error in DB request", error);
       res.status(500).json({ msg: "Error in DB request", err: error });
