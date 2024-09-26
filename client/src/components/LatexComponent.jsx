cc3c37090a926628112004a4c940be77e1b1befa
olidec <69003898+olidec@users.noreply.github.com>
2024-09-26 14:12:36 +0200

    now works from local

--- client/src/components/LatexComponent.jsx
+++ client/src/components/LatexComponent.jsx
@@ -1,18 +1,15 @@
-import { useEffect } from "preact/hooks";
+import React, { useEffect } from "react";
+// Import the LaTeXJSComponent from the local latex.js package
+import { LaTeXJSComponent } from "/node_modules/latex.js/dist/latex.mjs";
 
 const LatexComponent = ({ latexContent }) => {
   useEffect(() => {
-    import("https://cdn.jsdelivr.net/npm/latex.js/dist/latex.mjs")
-      .then((module) => {
-        const { LaTeXJSComponent } = module;
-        if (!customElements.get("latex-js")) {
-          customElements.define("latex-js", LaTeXJSComponent);
-        }
-      })
-      .catch((error) =>
-        console.error("Error loading LaTeXJSComponent:", error)
-      );
-  }, [latexContent]); // This runs only on mount
+    // Check if the 'latex-js' custom element is already defined
+    if (!customElements.get("latex-js")) {
+      // Define the 'latex-js' custom element using LaTeXJSComponent
+      customElements.define("latex-js", LaTeXJSComponent);
+    }
+  }, []); // This effect runs only once when the component is mounted
 
   return (
     <div>
